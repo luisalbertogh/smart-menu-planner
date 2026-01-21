cac0abe52ee6d389e2e86d135e19361838488f48
Luis Alberto Garcia Hernandez <luisalbertogh@hotmail.com>
2026-01-21 11:33:28 +0100

    Add cleanup step for old menu plans and recipes

---
+++ scripts/cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Keep only the 4 most recent menu plans
+cd "menu_plans" || exit 0 # Exit if directory does not exist
+ls -1 | sort -r | tail -n +5 | xargs -r rm -f --
+
+cd "recipes" || exit 0 # Exit if directory does not exist
+ls -1 | sort -r | tail -n +5 | xargs -r rm -rf --
