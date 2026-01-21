8e49c2e29ad3b4fd12f25ce318720452bb582819
Luis Alberto Garcia Hernandez <luisalbertogh@hotmail.com>
2026-01-21 11:53:32 +0100

    Fix script.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,5 +4,5 @@
 cd "menu_plans" || exit 0 # Exit if directory does not exist
 ls -1 | sort -r | tail -n +5 | xargs -r rm -f --
 
-cd "recipes" || exit 0 # Exit if directory does not exist
+cd "../recipes" || exit 0 # Exit if directory does not exist
 ls -1 | sort -r | tail -n +5 | xargs -r rm -rf --
